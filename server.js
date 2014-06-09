ca4ce225b00076c83dab83751b4e1114cedf4061
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2014-06-08 22:52:44 -0400

    Stub for data call

--- server.js
+++ server.js
@@ -5,9 +5,19 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
+
+
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
+
+// Orange - EFC station
+// http://www.wmata.com/rider_tools/pids/showpid.cfm?station_id=100
+
+app.get('/orange/efc', function(req, res) {
+  res.send('Vienna, BRD, 10, 20, New Carrollton, ARR, 5, 15');
+});
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
